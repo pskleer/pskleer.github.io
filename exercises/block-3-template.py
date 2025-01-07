87ef6526f7eeaa378da09029f443fdd614d30d69
pskleer <94305201+pskleer@users.noreply.github.com>
2025-01-07 12:51:49 +0100

    Add files via upload

---
+++ exercises/block-3-template.py
@@ -0,0 +1,63 @@
+##############################
+# 34V3A1 Mock Exam,  Block 3.#
+##############################
+
+# Grading (do not remove):
+# 1)  
+# 2)  
+# 3a)
+# 3b)  
+# 3c)
+
+snr = 1234567  # replace with your student number
+
+
+# Import any packages needed here
+
+
+"""
+Question 1): Write your solution below
+"""
+
+
+## Test input
+# x = np.array([-10,-5,-3,0,4,6,9])
+# t = 5
+# print(f(x,t)) 
+
+"""
+Question 2): Write your solution below
+"""
+
+
+## Test input
+# b = np.array([-1,1,2])
+# A = np.array([[-5,4,9],[1,1,1],[2,1,4]])
+
+# first, second = quantities(b,A)
+# print(first)
+# print(second)
+               
+"""
+Question 3a): Write your solution below
+"""
+
+
+
+"""
+Question 3b): Write your solution below
+"""
+
+
+
+"""
+Question 3c): Write your solution below
+"""
+
+
+
+## Part of test input
+# A = np.array([[1,2,3],[4,5,6]])
+# B = np.array([[3,3,5],[10,8,17]])
+
+

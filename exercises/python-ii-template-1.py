2aee98e1edbd12c82753efa38a1c147524d96051
pskleer <94305201+pskleer@users.noreply.github.com>
2025-12-01 18:07:21 +0100

    Add files via upload

---
+++ exercises/python-ii-template-1.py
@@ -0,0 +1,88 @@
+##################################
+####  35V3A1 Practice exam 1  ####
+##################################
+
+# Grading (do not remove):
+# 1)  
+# 2)  
+# 3a) 
+# 3b) 
+# 3c)  
+# 3d) 
+# 4a) 
+# 4b) 
+# 4c)  
+
+
+snr = 1234567  # replace with your student number
+
+
+# Import any packages needed here by uncommenting (change alias if you want)
+
+# import numpy as np
+# import scipy.optimize as optimize
+# import scipy.stats as stats 
+# from scipy.optimize import linprog
+# import matplotlib.pyplot as plt
+
+
+"""
+Question 1): Write your solution below
+"""
+
+
+"""
+Question 2): Write your solution below
+"""
+
+# Test input
+# x = np.array([-1,1,2])
+# A = np.array([[-5,-10,9],[1,3,0],[2,1,4]])
+
+# first, second = quantities(x,A)
+# print(first)
+# print(second)
+
+
+"""
+Question 3a): Write your solution below
+"""        
+    
+"""
+Question 3b): Write your solution below
+"""
+
+"""
+Question 3c): Write your solution below
+"""
+
+"""
+Question 3d): Write your solution below
+"""
+
+
+"""
+Question 4a): Write your solution below
+"""
+
+"""
+Question 4b): Write your solution below
+"""
+D = np.array([
+ [-2,         4.93111585],
+ [-1.44444444,  0.24990997],
+ [-0.88888889, -0.05682958],
+ [-0.33333333,  1.02338187],
+ [ 0.22222222,  4.14945194],
+ [ 0.77777778,  7.15688363],
+ [ 1.33333333,  8.70971831],
+ [ 1.88888889,  9.09293372],
+ [ 2.44444444,  6.50802039],
+ [ 3,         -0.06842749]])
+ 
+d = 4
+
+
+"""
+Question 4c): Write your solution below
+"""
---
+++ exercises/python-ii-template-2.py
@@ -0,0 +1,66 @@
+##################################
+####  35V3A1 Practice exam 2  ####
+##################################
+
+# Grading (do not remove):
+# 1)  
+# 2)  
+# 3a) 
+# 3b) 
+# 4a) 
+# 4b) 
+# 4c)  
+
+
+snr = 1234567  # replace with your student number
+
+
+# Import any packages needed here by uncommenting (change alias if you want)
+
+# import numpy as np
+# import scipy.optimize as optimize
+# import scipy.special as special
+# import scipy.stats as stats 
+# import matplotlib.pyplot as plt
+
+
+"""
+Question 1): Write your solution below
+"""
+
+## Test input
+# x = np.array([-5,-0.5,0.5,4,9])
+# print(f(x)) 
+
+"""
+Question 2): Write your solution below
+"""
+
+
+## Test input
+# x  = np.array([-1,1,2])
+# A  = np.array([[-5,4,9],[1,1,1],[2,1,4]])
+
+# first, second = quantities(x,A)
+# print(first)
+# print(second)
+
+"""
+Question 3a): Write your solution below
+"""        
+
+"""
+Question 3b): Write your solution below
+"""
+
+"""
+Question 4a): Write your solution below
+"""
+
+"""
+Question 4b): Write your solution below
+"""
+
+"""
+Question 4c): Write your solution below
+"""
